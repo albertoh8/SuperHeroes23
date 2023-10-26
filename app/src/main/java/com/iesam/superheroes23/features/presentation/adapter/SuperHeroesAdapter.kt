0e9b5ab0bd38058e30d0873287a3b5a813c63232
albertoh8 <albertoherrero888@gmail.com>
2023-10-26 19:17:33 +0200

    Add DiffUtil and Filter Heroe by name

--- app/src/main/java/com/iesam/superheroes23/features/presentation/adapter/SuperHeroesAdapter.kt
+++ app/src/main/java/com/iesam/superheroes23/features/presentation/adapter/SuperHeroesAdapter.kt
@@ -2,27 +2,23 @@ package com.iesam.superheroes23.features.presentation.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.iesam.superheroes23.R
 import com.iesam.superheroes23.features.domain.GetSuperHeroesFeedUseCase
+import com.iesam.superheroes23.features.presentation.SuperHeroDiffUtil
 
-class SuperHeroesAdapter() : RecyclerView.Adapter<SuperHeroViewHolder>() {
+class SuperHeroesAdapter() : ListAdapter<GetSuperHeroesFeedUseCase.SuperHeroList,SuperHeroViewHolder>(SuperHeroDiffUtil()) {
 
-    private val list : MutableList<GetSuperHeroesFeedUseCase.SuperHeroList> = mutableListOf()
 
-    fun setList(heroes  : List<GetSuperHeroesFeedUseCase.SuperHeroList>){
-        list.clear()
-        list.addAll(heroes)
-        notifyDataSetChanged()
-    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.view_superhero_feed,parent,false)
         return SuperHeroViewHolder(view)
     }
 
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int = currentList.size
 
     override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
-        holder.bind(list.get(position))
+        holder.bind(currentList.get(position))
     }
 }
